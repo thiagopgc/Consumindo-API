c652182b2a363ce3e4c1723963c0cd8bbcc9c921
thiagopgc <thiagopereiragc@yahoo.com.br>
2022-04-13 15:54:09 -0300

    criado o index.js para percorrer o array de objetos da api usando o metodo map e retornar o valor do input através do evento de click do botão buscar.

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const url =
-  "https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=camiseta&source=nanook";
 
-function usandoApi() {
-  axios
-    .get(url)
-    .then((response) => {
-      const data = response.data;
-      monstrandoApi.textContent = JSON.stringify(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-usandoApi();
