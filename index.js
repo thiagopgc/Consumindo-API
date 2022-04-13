070cb5febfad4626b54f4ae78d5861d1f33113f9
thiagopgc <thiagopereiragc@yahoo.com.br>
2022-04-13 15:59:41 -0300

    criado o index.js para percorrer o array de objetos da api usando o metodo map e retornar o valor do input através do evento de click do botão buscar.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const botao = document.getElementById("botao");
+botao.addEventListener("click", (e) => {
+  e.preventDefault();
+  const valor = document.querySelector("#produto").value;
+  const url = `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${valor}&source=nanook`;
 
+  axios
+    .get(url)
+    .then((resp) => {
+      const retornaProduto = document.getElementById("produtosRetornados");
+      let produtos = resp.data.products;
+      retornaProduto.innerHTML = "";
+
+      produtos.map((item) => {
+        console.log(item);
+        retornaProduto.innerHTML += `
+            <div class="card">
+                <p class="text">Nome: ${item.name}</p>
+                <p>ID: ${item.id}</p>
+                <p>Pontuação: ${item._meta.score}</p>
+                <p>Visitas ao produto: ${item._meta.visitsClickCount}</p>
+            </div>
+            `;
+      });
+    })
+    .catch((e) => e.message);
+});
